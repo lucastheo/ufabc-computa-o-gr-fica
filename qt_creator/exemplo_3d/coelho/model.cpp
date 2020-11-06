6fff428f546a44b45bed473958c5a6303948af5f
L Theodoro <lucastheo.g.a@gmail.com>
2020-11-06 14:35:42 -0300

    Mais um parte que faltou

--- qt_creator/exemplo_3d/coelho/model.cpp
+++ qt_creator/exemplo_3d/coelho/model.cpp
@@ -2,5 +2,40 @@
 
 Model::Model()
 {
+    shaderProgram.resize(fragmentShaderFileName.size());
+}
+
+void Model::readOFFFile(QString &fileName ){
+    QFile data(fileName);
+    data.open(QFile::ReadOnly);
+    if( !data.isOpen() ){
+        qWarning("Cannot open file");
+        return;
+    }
+
+    QTextStream stream(&data);
+    QString notUsed;
+
+    stream >> notUsed; //OFF
+    stream >>numVertives >> numFaces >> notUsed;
+
+    vertices.resize(numVertives);
+    indices.resize(3*numFaces);
+
+    float x,y,z;
+    for( size_t i{0}; i < numVertives; i++ ){
+        stream >> x >> y >> z;
+        vertices[i] = QVector4D( x , y , z , 1);
+    }
+    int a,b,c;
+    for(size_t i{0};numFaces;i++){
+        stream >> notUsed >> a >> b >> c;
+        indices[i*3 + 0] = a;
+        indices[i*3 + 1] = b;
+        indices[i*3 + 2] = c;
+    }
+    //rescaleModel();
+    data.close();
+
 
 }
--- qt_creator/exemplo_3d/coelho/model.h
+++ qt_creator/exemplo_3d/coelho/model.h
@@ -1,11 +1,40 @@
 #ifndef MODEL_H
 #define MODEL_H
-
+#include<vector>
+#include<QVector4D>
+#include<QVector3D>
+#include<QString>
+#include<QFile>
+#include<QTextStream>
 
 class Model
 {
 public:
     Model();
+    ~Model() = default;
+    
+    std::vector<QVector4D> vertices;
+    std::vector<unsigned> indices; 
+    unsigned numVertives{0};
+    unsigned numFaces;
+    
+    unsigned vao{0};
+    unsigned vboVertice{0};
+    unsigned eboIndices{0};
+    
+    void readOFFFile( QString &);
+
+    void computeBBox();
+    void rescalModel();
+    QVector3D centrolidBB, minBB, maxBB;
+    float diagonalBB{0.0f};
+
+    int currentShader{0};
+    std::vector<QString> vertexShaderFileName = {":shaders/vzdepht.glsl"};
+    std::vector<QString> fragmentShaderFileName = {":shaders/fzdepht.glsl"};
+
+    std::vector<unsigned> shaderProgram;
+
 };
 
 #endif // MODEL_H
